7b60e5281829010ad920650b4cc82e4d1e0ea745
Thebug31 <ortizvalencia31@gmail.com>
2026-04-09 20:13:29 -0500

    commit 5: mejora switch case con ciclo y validaciones

--- bin/cli.dart
+++ bin/cli.dart
@@ -8,30 +8,40 @@ void main() {
   List<int> stock = [8, 3, 15, 0];
   List<double> precios = [220000, 200000, 180000, 170000];
 
+  int opcion = 2;
+  int juegoElegido = 1;
+  int cantidad = 5;
+
   print('=== TIENDA DE VIDEOJUEGOS ===\n');
 
   for (int i = 0; i < juegos.length; i++) {
     print('[$i] ${juegos[i]} - Stock: ${stock[i]} - Precio: \$${precios[i]}');
   }
 
-  int opcion = 2;
-
   switch (opcion) {
     case 1:
       print('\nModo: Ver inventario');
+      for (int i = 0; i < juegos.length; i++) {
+        print('  ${juegos[i]} - Stock: ${stock[i]}');
+      }
       break;
     case 2:
       print('\nModo: Vender juego');
+      print('Juego seleccionado: ${juegos[juegoElegido]}');
       break;
     case 3:
       print('\nModo: Agregar stock');
+      for (int i = 0; i < juegos.length; i++) {
+        if (stock[i] == 0) {
+          print('  ${juegos[i]} - AGOTADO');
+        } else {
+          print('  ${juegos[i]} - Stock: ${stock[i]}');
+        }
+      }
       break;
     default:
       print('\nOpción inválida');
   }
-
-  int juegoElegido = 1;
-  int cantidad = 5;
 
   try {
     if (juegoElegido < 0 || juegoElegido >= juegos.length) {
